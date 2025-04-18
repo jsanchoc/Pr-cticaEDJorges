12590f86cec41c2ea7ec5404d9efe12a61c1df7d
Jorge Sancho Calzada <jorgesanchocalzada@MacBook-Pro-de-Jorge.local>
2025-04-18 20:08:40 +0200

    Función eliminar

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 # IMPORTACIÓN DE LIBRERÍAS
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_cors import CORS
 from JGVutils import SQLiteConnection
 
@@ -44,12 +44,51 @@ def agregar_pelicula():
 
     return "Película agregada correctamente."
 
-# RUTA PARA MOSTRAR TODO
+# RUTA PARA MOSTRAR TODO 
 @application.route("/mostrar", methods=["GET"])
 def mostrar_peliculas():
+    # Recogemos los posibles argumentos
+    codigo = request.args.get("codigo")
+    genero = request.args.get("genero")
+    edad = request.args.get("edad")
+    director = request.args.get("director")
+    titulo = request.args.get("titulo")
+
+    # Creamos la base de la consulta
+    query = "SELECT * FROM peliculas WHERE 1=1"
+    valores = []
+
+    if codigo:
+        query += " AND codigo = ?"
+        valores.append(codigo)
+    if genero:
+        query += " AND genero = ?"
+        valores.append(genero)
+    if edad:
+        query += " AND edad = ?"
+        valores.append(edad)
+    if director:
+        query += " AND director = ?"
+        valores.append(director)
+    if titulo:
+        query += " AND titulo = ?"
+        valores.append(titulo)
+
     conexion = SQLiteConnection("Database1.db")
-    peliculas = conexion.execute_query("SELECT * FROM peliculas")
+    peliculas = conexion.execute_query(query, valores)
+
+    print(f"CONSULTA: {query} - VALORES: {valores}")
     return render_template("mostrar.html", peliculas=peliculas)
+
+@application.route("/eliminar/<int:codigo>", methods=["GET"])
+def eliminar_pelicula(codigo):
+    conexion = SQLiteConnection("Database1.db")
+    conexion.execute_query(
+        "DELETE FROM peliculas WHERE codigo = ?",
+        [codigo],
+        commit=True
+    )
+    return redirect(url_for("mostrar_peliculas"))
 
 
 if __name__ == "__main__":
